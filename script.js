de11d7591db1d2d7412066e24a9fc5afb0e3d6d9
Chaiyapong_Japan <japanouan88@gmail.com>
2025-10-22 12:51:38 +0700

    Add initial PEWPEW Game files
    Added index.html, script.js, and style.css to set up the basic structure of the PEWPEW Game. The JavaScript implements player movement, shooting, enemy spawning, and basic UI, while the CSS styles the canvas and centers it on the page.

---
+++ script.js
@@ -0,0 +1,204 @@
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class InputHandler{
+        constructor(game){
+            this.game = game;
+            window.addEventListener('keydown', e => {
+                if ( ( e.key === 'ArrowDown' || e.key === 'ArrowUp' ) && this.game.keys.indexOf(e.key) === -1 ){
+                    this.game.keys.push(e.key);
+                }else if ( e.key === ' ' ) {
+                    this.game.player.shootTop();
+                }
+            })
+            window.addEventListener('keyup', e => {
+                if ( this.game.keys.indexOf(e.key) > -1 ){
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+                }               
+            })
+        }
+    }
+
+    class Projectile{
+        constructor(game, x, y){
+            this.game = game;
+            this.x = x;
+            this.y = y;
+            this.width = 10;
+            this.height = 3;
+            this.speed = 3;   
+            this.markForDeletion = false;
+        }
+        update(){
+            this.x += this.speed;
+            if ( this.x > this.game.width * 0.8 ) this.markForDeletion = true;
+        }
+        draw(context){
+            context.fillStyle = 'yellow';
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    class Particle{
+
+    }
+    class Player{
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 120;
+            this.x = 20;
+            this.y = 120;
+            this.speedY = 0;
+            this.speedX = 0;
+            this.maxSpeed = 2;
+            this.projectiles = [];
+        }
+        update(){
+            if ( this.game.keys.includes('ArrowUp') ) this.speedY = -this.maxSpeed;
+            else if ( this.game.keys.includes('ArrowDown') ) this.speedY = this.maxSpeed;
+            else this.speedY = 0;
+            this.y += this.speedY;
+            //handle projectiles
+            this.projectiles.forEach(projectile => {
+                projectile.update();
+            })
+            this.projectiles = this.projectiles.filter(projectile => !projectile.markForDeletion);
+        }
+        draw(context){
+            context.fillStyle = 'white';
+            context.fillRect(this.x, this.y, this.width, this.height);
+            this.projectiles.forEach(projectile => {
+                projectile.draw(context);
+            })
+        }
+        shootTop(){
+            if ( this.game.ammo > 0 ) {
+                this.projectiles.push(new Projectile(this.game, this.x, this.y));
+                this.game.ammo--;
+            }
+        }
+    }
+    class Enemy{
+        constructor(game){
+            this.game = game;
+            this.x = this.game.width;
+            this.speedX = Math.random() * -1.5 - 0.5;
+            this.markForDeletion = false;
+        }
+        update(){
+            this.x += this.speedX;
+            if ( this.x + this.width < 0 ) this.markForDeletion = true;
+        }
+        draw(context){
+            context.fillStyle = 'red';
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    class Anglerl extends Enemy {
+        constructor(game){
+            super(game);
+            this.width = 228 * 0.2;
+            this.height = 169 * 0.2;
+            this.y = Math.random() * (this.game.height * 0.9 - this.height);
+        }
+    }
+
+    class Layer{
+
+    }
+
+    class Background{
+
+    }
+
+    class UI{
+        constructor(game){
+            this.game = game;
+            this.fontSize = 25;
+            this.fontFamily = 'Helvetica';
+            this.color = 'yellow';
+        }
+        draw(context){
+            //ammo
+            context.fillStyle = this.color;
+            for (let i = 0; i < this.game.ammo; i++){
+                context.fillRect(20 +5 * i, 50, 3, 20);
+            }
+        }
+    }
+    class Game{
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.ui = new UI(this);
+            this.keys = [];
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
+            this.ammo = 20;
+            this.maxammo = 50;
+            this.ammoTimer = 0;
+            this.ammoInterval = 500;
+            this.gameOver = false;
+        }
+        update(deltaTime){
+            this.player.update();
+            if (this.ammoTimer > this.ammoInterval){
+                if (this.ammo < this.maxammo) this.ammo++;
+                this.ammoTimer = 0;
+            }else{
+                this.ammoTimer += deltaTime;
+            }
+            
+            this.enemies.forEach((enemy)=>{
+                enemy.update();
+            });
+            this.enemies = this.enemies.filter(enemy => !enemy.markForDeletion);
+            if (this.enemyTimer > this.enemyInterval && !this.gameOver){
+                this.addEnemy();
+                console.log('add enemy');
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime
+            }
+            // console.log(this.deltaTime);
+            
+        }
+        draw(context){
+            this.player.draw(context);
+            this.ui.draw(context);
+            this.enemies.forEach((enemy)=>{
+                enemy.draw(context);
+            });
+        }
+        addEnemy(){
+            this.enemies.push(new Anglerl(this));
+            // console.log(this.enemies);
+        }
+        checkCollision(rect1, rect2){
+            return rect1.x < rect2.x + rect2.width &&
+                rect1.x + rect1.width > rect2.x &&
+                rect1.y < rect2.y + rect2.height &&
+                rect1.y + rect1.height > rect2.y
+        }
+    }
+    
+    const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
+    //animate loop
+    function animate(timestamp){
+        const deltaTime = timestamp - lastTime
+        lastTime = timestamp;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.update(deltaTime);
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate(0);
+});
